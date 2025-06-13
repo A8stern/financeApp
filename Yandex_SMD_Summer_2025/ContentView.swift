2ed588ac8372ddaf8bc6ed4361f450a3dfb9d9c6
Gleb <kovgleb04@gmail.com>
2025-06-14 02:21:17 +0300

    First commit

---
+++ Yandex_SMD_Summer_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Yandex_SMD_Summer_2025
+//
+//  Created by Kovalev Gleb on 14.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, yandex!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Yandex_SMD_Summer_2025/Yandex_SMD_Summer_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Yandex_SMD_Summer_2025App.swift
+//  Yandex_SMD_Summer_2025
+//
+//  Created by Kovalev Gleb on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Yandex_SMD_Summer_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
