bcb5584b66be6cd5c3e48ee6b23d7d21bb06dc84
Gleb <kovgleb04@gmail.com>
2025-07-04 23:54:59 +0300

    fuzzy search fix

--- Yandex_SMD_Summer_2025/Extensions/FuzzySearchableProtocol/FuzzyModels.swift
+++ Yandex_SMD_Summer_2025/Extensions/FuzzySearchableProtocol/FuzzyModels.swift
@@ -9,11 +9,9 @@ import Foundation
 
 struct FuzzySearchCharacter {
     let content: String
-    // normalised content is referring to a string that is case- and accent-insensitive
     let normalisedContent: String
 }
 
-/// FuzzySearchString is just made up by multiple characters
 struct FuzzySearchString {
     var characters: [FuzzySearchCharacter]
 }
@@ -21,9 +19,4 @@ struct FuzzySearchString {
 struct FuzzySearchMatchResult {
     let weight: Int
     let matchedParts: [NSRange]
-}
-
-class FuzzyCache {
-    var hash: Int?
-    var lastTokenization = FuzzySearchString(characters: [])
 }
